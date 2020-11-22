c352f868af9a6cedc02574b3314576dfa0bc1272
ssynx <sa.belli@hotmail.it>
2020-11-22 23:58:03 +0100

    Delete internal-package.bat

--- internal-package.bat
+++
@@ -1,5 +0,0 @@
-cd whork/webapp
-mvn assembly:single
-cd ../..
-mv whork/webapp/target/desktopapp-0.0.0-jar-with-dependencies.jar .
-ren desktopapp-0.0.0-client-jar-with-dependencies.jar whork-desktop-%APPVEYOR_REPO_TAG_NAME%-win32.jar
