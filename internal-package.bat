159ae79a757e2cbbdca98dbb44b5332fcf697f87
ssynx <sa.belli@hotmail.it>
2020-11-22 23:03:46 +0100

    update internal-package.bat

--- internal-package.bat
+++ internal-package.bat
@@ -3,4 +3,3 @@ mvn assembly:single
 cd ../..
 mv whork/webapp/target/desktopapp-0.0.0-jar-with-dependencies.jar .
 ren desktopapp-0.0.0-client-jar-with-dependencies.jar whork-desktop-%APPVEYOR_REPO_TAG_NAME%-win32.jar
-appveyor PushArtifact whork-desktop-%APPVEYOR_REPO_TAG_NAME%-win32.jar
